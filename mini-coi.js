00b3b69c35c41d0218cad3a93a2ec010f66cab0e
Martin Morgan <mtmorgan.xyz@gmail.com>
2026-06-08 14:54:34 -0400

    exclude '/birdcafe/ from mini-coi.js

--- mini-coi.js
+++ mini-coi.js
@@ -3,26 +3,30 @@
 (({ document: d, navigator: { serviceWorker: s } }) => {
   if (d) {
     const { currentScript: c } = d;
-    s.register(c.src, { scope: c.getAttribute('scope') || '.' }).then(r => {
+    s.register(c.src, { scope: c.getAttribute('scope') || '.' }).then((r) => {
       r.addEventListener('updatefound', () => location.reload());
       if (r.active && !s.controller) location.reload();
     });
-  }
-  else {
+  } else {
     addEventListener('install', () => skipWaiting());
-    addEventListener('activate', e => e.waitUntil(clients.claim()));
-    addEventListener('fetch', e => {
+    addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+    addEventListener('fetch', (e) => {
       const { request: r } = e;
+      // Skip isolation for the birdcafe path
+      if (r.url.includes('/birdcafe/')) return;
+
       if (r.cache === 'only-if-cached' && r.mode !== 'same-origin') return;
-      e.respondWith(fetch(r).then(r => {
-        const { body, status, statusText } = r;
-        if (!status || status > 399) return r;
-        const h = new Headers(r.headers);
-        h.set('Cross-Origin-Opener-Policy', 'same-origin');
-        h.set('Cross-Origin-Embedder-Policy', 'require-corp');
-        h.set('Cross-Origin-Resource-Policy', 'cross-origin');
-        return new Response(body, { status, statusText, headers: h });
-      }));
+      e.respondWith(
+        fetch(r).then((r) => {
+          const { body, status, statusText } = r;
+          if (!status || status > 399) return r;
+          const h = new Headers(r.headers);
+          h.set('Cross-Origin-Opener-Policy', 'same-origin');
+          h.set('Cross-Origin-Embedder-Policy', 'require-corp');
+          h.set('Cross-Origin-Resource-Policy', 'cross-origin');
+          return new Response(body, { status, statusText, headers: h });
+        })
+      );
     });
   }
 })(self);
